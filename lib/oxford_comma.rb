8e27df3f023886887d6947b1bfe0d56a3c54d84a
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-06 19:50:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+ if array.length == 1
+   return array.join
+ elsif array.length == 2
+   return array.join(" and ")
+ elsif array.length == 3
+    final = array[0..1].join(", ")
+    final += ", and "+ array[2]
+  else
+    array[0...-1].join(", ") << ", and #{array[-1]}"
 
+ end
 end
