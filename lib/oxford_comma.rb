274dedbe1cc7ae843742c370770189a860483503
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-06 19:26:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,9 @@ def oxford_comma(array)
  elsif array.length == 2
    return array.join(" and ")
  elsif array.length == 3
-
+    final = array[0..1].join(", ")
+    final += array[2]
+  else
+    array[0..array.length-1].join(", ")
  end
 end
